dff417ddbcd8fa896104c000c87696c81ed67d01
Leonardo Galvão <leonardogalvao_@hotmail.com>
2025-09-24 23:46:35 -0300

    first commit

---
+++ src/test/java/br/com/empresa/gerenciadordetarefas/GerenciadordetarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.empresa.gerenciadordetarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadordetarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
